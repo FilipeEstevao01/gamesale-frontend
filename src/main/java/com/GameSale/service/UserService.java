448d8fb336dacc9570fb1941e2d05d9e03f011f6
Filipe <filipedev0@gmail.com>
2026-08-18 19:37:34 -0300

    Foi feito o comentario do public que permite outas classes usarem esta class

--- src/main/java/com/GameSale/service/UserService.java
+++ src/main/java/com/GameSale/service/UserService.java
@@ -11,15 +11,19 @@ public class UserService {
 
     // Aqui criando uma variavel.
     // O usuario e que converça com o banco.
+
     private UsuarioRepository usuarioRepository;
 
     //Isso e a injeção de dependecias por construtor
+
     public UserService(UsuarioRepository usuarioRepository) {
         this.usuarioRepository = usuarioRepository;
     }
 
-
+    //O public esta falando basicamente que esta class pode ser usada em outras class
     public Usuario getusuarioBySenha(String senha) {
+
+
         return (Usuario) usuarioRepository.findByEmail(senha)
                 .orElseThrow(() -> new RuntimeException("User not found"));
     }
