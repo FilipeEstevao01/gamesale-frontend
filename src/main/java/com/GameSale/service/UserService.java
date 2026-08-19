3fbfb20b25d4dc7e492097d9ac7604da3672d84c
Filipe <filipedev0@gmail.com>
2026-08-19 11:12:23 -0300

    Foi criado na Service algumas regas de negocio para o GameSale e comentado o codigo para fins de estudo. Revisar depois.

--- src/main/java/com/GameSale/service/UserService.java
+++ src/main/java/com/GameSale/service/UserService.java
@@ -1,4 +1,6 @@
 
+//Na Service é onde normalmente colocamos as regras de negócio.
+
 //Isso informa o pacote em que sua class esta localizada
 package com.GameSale.service;
 
@@ -16,17 +18,21 @@ public class UserService {
     //Meu UserService possui um UsuarioRepository para acessar os dados dos usuários.
     private final UsuarioRepository usuarioRepository;
 
+    //Aqui esta o contrutor
     public UserService(UsuarioRepository usuarioRepository) {
         this.usuarioRepository = usuarioRepository;
     }
 
+    //Aqui foi criado o metodo buscar por e-mail
     public boolean buscarPorEmail(String email) {
 
         return usuarioRepository.findByEmail(email);
     }
 
+    //Aqui e feito o cadastro de usuario
     public Usuario cadastrarUsuario(Usuario usuario) {
 
+        //Agora vem a regra de de negocio
         if (usuarioRepository.findByEmail(usuario.getEmail())) {
             throw new RuntimeException("Email já cadastrado");
         }
@@ -42,6 +48,7 @@ public class UserService {
                         );
     }
 
+    //metodo para excluir usuario
     public  void excluir(Integer id) {
 
         Usuario usuario = buscarPorId(id);
