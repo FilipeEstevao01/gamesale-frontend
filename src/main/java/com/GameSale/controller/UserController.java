80b8dee9c4deefafa2966969d04fc77a12d2dea9
Filipe <filipedev0@gmail.com>
2026-08-20 07:54:20 -0300

    Agora vou tentar fazer para outras tabelas

--- src/main/java/com/GameSale/controller/UserController.java
+++ src/main/java/com/GameSale/controller/UserController.java
@@ -10,4 +10,31 @@ import java.util.List;
 @RestController
 @RequestMapping()
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<Usuario> findAll() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/email/{email}")
+    public String findByEmail(@PathVariable String email) {
+        return userService.findByEmail(email);
+    }
+
+    @GetMapping("/name/{name}")
+    public String findByname(@PathVariable String name) {
+        return userService.findByEmail(name);
+    }
+
+    @DeleteMapping("/email/{email}")
+        public void deleteByEmail (@PathVariable String email) {
+        userService.deleteByEmail(email);
+    }
+
 }
