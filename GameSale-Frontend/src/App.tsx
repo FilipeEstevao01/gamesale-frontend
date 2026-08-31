e97a167fe7ed2b57574ce1e05186a0b9e31146ce
Filipe <179870372+FilipeEstevao01@users.noreply.github.com>
2026-08-31 18:00:27 -0300

    Foi tentado fazer a tentativa de mexher na tela de login

--- GameSale-Frontend/src/App.tsx
+++ GameSale-Frontend/src/App.tsx
@@ -5,7 +5,20 @@ function App() {
 
   return (
     <>
+    <h1> Gerenciador de usuario</h1>
+
+       <label className='name'>
+        Nome
+       </label>
+      
+      <div className='textnome'>
+       <input type="text" 
+       placeholder='Digite seu nome'
+       />
+       </div>
+
     
+
     </>
   )
 }
