4f19cf4cc9555e29c664fcc2c35999cd91e4f197
Filipe <179870372+FilipeEstevao01@users.noreply.github.com>
2026-09-01 17:31:25 -0300

    Criando e separando o front do back

---
+++ GameSale-Frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1> Gerenciador de usuario</h1>
+
+       <label className='name'>
+        Nome
+       </label>
+      
+      <div className='textnome'>
+       <input type="text" 
+       placeholder='Digite seu nome'
+       />
+       </div>
+
+    
+
+    </>
+  )
+}
+
+export default App
