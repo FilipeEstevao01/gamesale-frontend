2a6d3cf229447f22bd172663f2ae0eb3662850c7
Filipe <179870372+FilipeEstevao01@users.noreply.github.com>
2026-09-11 17:06:50 -0300

    Estou tentndo estilizar a tela de login e fazer e fazer o butoon

--- GameSale-Frontend/src/App.tsx
+++ GameSale-Frontend/src/App.tsx
@@ -3,65 +3,100 @@ import Cabesalho from './components/Cabesalho';
 
 
 function App() {
-  return (
-    <>
+    return (
+        <>
 
 
-           <Cabesalho />
+            <Cabesalho />
 
 
- <div className="card">   
+            <div className="card">
 
-        <div className="labelnome">
-            <label htmlFor="nome">
-                Nome
-            </label>
-        </div>
 
-            <div>
-                <input type="nome"
-                    placeholder="Digite seu nome..."
-                />
+                <div className="formulario">
+                    <div className="labelnome">
+                        <label htmlFor="nome">
+                            Nome
+                        </label>
+                    </div>
+
+                    <div>
+                        <input type="nome"
+                            placeholder="Digite seu nome..."
+                        />
+                    </div>
+
+
+                    <br />
+
+
+
+                    <div className="labelemail">
+                        <label htmlFor="email">
+                            Email
+                        </label>
+                    </div>
+
+
+
+
+
+
+
+                    <div>
+                        <input type="email"
+                            placeholder="Digite seu email..."
+                        />
+                    </div>
+
+
+
+
+
+
+                    <br />
+
+                    <div className="labeltelefone">
+                        <label htmlFor="telefone">
+                            Telefone
+                        </label>
+                    </div>
+
+                    <div>
+                        <input type="telefone"
+                            placeholder="Digite seu telefone..."
+                        />
+                    </div>
+                </div>
+
+
+               
+                
+                <div>
+                    <a href="https://seusite.com.br" className="botao-enviar"></a>
+                    <button className="botao">
+                        Enviar
+                    </button>
+                </div>
+
+
+                <div>
+                    <a href="https://seusite.com.br" className="botao-cancelar"></a>
+                    <button className="botao">
+                        Cancelar
+                    </button>
+                </div>
+
+                
+             
+
+
             </div>
 
 
-            <br />
 
-
-
-        <div className="labelemail">
-            <label htmlFor="email">
-                Email
-            </label>
-        </div>
-
-
-            <div>
-                <input type="email"
-                    placeholder="Digite seu email..."
-                />
-            </div>
-
-
-            <br />
-
-        <div className="labeltelefone">
-            <label htmlFor="telefone">
-                Telefone
-            </label>
-        </div>
-
-            <div>
-                <input type="telefone"
-                    placeholder="Digite seu telefone..."
-                />
-            </div>
-
-
-            </div>
-
-    </>
-  )
+        </>
+    )
 }
 
 export default App
